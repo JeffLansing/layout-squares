9a2798816fa1f0c22de4fa80c45c378de05c5223
JeffLansing <lansing2357@gmail.com>
2024-02-29 15:29:42 -0800

    created example data structures

---
+++ R/doc_example_far.R
@@ -0,0 +1,12 @@
+#' example_far
+#'
+#' Description.
+#'
+#' @format A data array with 3 rows and 4 variables in 12 layers:
+#' \describe{
+#' \item{1 layer for each face of a rombic dodecahedron}
+#' \item{1 variable for each corner of a face}
+#' \item{a name, and an x and y coordinate for each corner}
+#' }
+#' @source Source
+"example_far"
---
+++ R/doc_example_rar.R
@@ -0,0 +1,11 @@
+#' example_rar
+#'
+#' Description.
+#'
+#' @format An array with 24 rows and 4 variables:
+#' \describe{
+#' \item{one row for each ridge of a rombic dodecahedron}
+#' \item{two face ids associated with 2 vertex ids in each row}
+#' }
+#' @source Source
+"example_rar"
