459d1547235df0bafdac4945f0b9e9ad14c29cfa
JeffLansing <lansing2357@gmail.com>
2024-02-29 16:34:29 -0800

    test adjust face

--- tests/testthat/test-adjust_face.R
+++ tests/testthat/test-adjust_face.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("adjust face", {
+  far <- example_far
+  rar <- example_rar
+  expected  <- c(14,1,0,10,1,1,9,2,1,13,2,0) %>%
+    array(dim = c(3,4), dimnames = list(c('nm', 'x', 'y'), letters[1:4]))
+  expect_identical(adjust_face(far, rar, 1, 5)[,,5], expected)
 })
